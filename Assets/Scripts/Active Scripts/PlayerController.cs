f36bea08ba2e64ffffd6c3782401e590d6fc115e
srsbsns5 <elyzaescano@gmail.com>
2022-07-08 17:04:05 +0800

    player jumpy

--- Assets/Scripts/Active Scripts/PlayerController.cs
+++ Assets/Scripts/Active Scripts/PlayerController.cs
@@ -6,9 +6,24 @@ public class PlayerController : MonoBehaviour
 {
     public CharacterController charaController;
     public float speed = 12f;
+    public float jumpSpeed = 5f;
+
+    [Header ("GroundCheck")]
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    bool isGrounded;
     public float gravity = -9.81f;
     Vector3 velocity;
-    void Update(){
+    void Update()
+    {
+        #region player physics
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
@@ -17,7 +32,14 @@ public class PlayerController : MonoBehaviour
         charaController.Move(move * speed * Time.deltaTime);
 
         velocity.y += gravity * Time.deltaTime;
-
         charaController.Move(velocity * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpSpeed * -2f * gravity);
+        }
+
+        #endregion
+
     }
 }
