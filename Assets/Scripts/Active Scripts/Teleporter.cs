b23ff09326caed9920e82a7fc94cc97334977092
srsbsns5 <elyzaescano@gmail.com>
2022-07-19 23:36:17 +0800

    AssetManagement
    - removed some extra assets
    - added more scriptable objects

--- Assets/Scripts/Active Scripts/Teleporter.cs
+++ Assets/Scripts/Active Scripts/Teleporter.cs
@@ -4,18 +4,24 @@ using UnityEngine;
 
 public class Teleporter : MonoBehaviour
 {
-    private RaycastHit hit;
-    public LayerMask playerLayer;
-    public Transform destination;
-    GameObject objectToTeleport;
-
+    public GameObject destination;
+    [SerializeField] GameObject objectToTeleport;
     private void OnTriggerEnter(Collider other) 
     {
         if (other.gameObject.tag == "Player")
         {
             objectToTeleport = other.gameObject;
-            print(objectToTeleport);
-            objectToTeleport.transform.position = destination.transform.position;    
+            print(objectToTeleport.name);
+            print(destination.name);
+            StartCoroutine(Teleport());             
         }
     }
+
+    IEnumerator Teleport()
+    {
+        yield return new WaitForSeconds(1);
+        Debug.Log("TELEPORT PLEASE");
+        objectToTeleport.transform.position = destination.transform.position;   
+    }
+    
 }
