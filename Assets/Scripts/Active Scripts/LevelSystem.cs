b37ae1b936af0d5b1e4e487cb2e70e61d94db6ce
srsbsns5 <elyzaescano@gmail.com>
2022-07-22 16:50:25 +0800

    Experience and Level Up
    - Temporary UI
    - Scripting Calculations

---
+++ Assets/Scripts/Active Scripts/LevelSystem.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class LevelSystem : MonoBehaviour
+{
+    public int level;
+    public float currentXP;
+    public float requiredXP;
+
+    private float lerpTimer;
+    private float delayTimer;
+
+    [Header ("UI")]
+    public Image frontXPBar;
+    public Image backXPBar;
+    public Image xpBarHolder;
+
+    [Header ("Calculations")]
+    public float additionMultiplier = 300;
+    public float powerMultiplier = 2;
+    public float divisionMultiplier = 7;
+
+    void Start()
+    {
+        frontXPBar.fillAmount = currentXP /requiredXP;
+        backXPBar.fillAmount = currentXP /requiredXP;
+
+        requiredXP = CalculateRequiredXP();
+    }
+
+    void Update()
+    {
+        UpdateXPUI();
+
+        if(currentXP > requiredXP)
+        {
+            LevelUp();
+        }
+
+    }
+
+    void UpdateXPUI()
+    {
+        float xpFraction = currentXP / requiredXP;
+        float FXP = frontXPBar.fillAmount;
+
+        if (FXP < xpFraction)
+        {
+            delayTimer += Time.deltaTime;
+            backXPBar.fillAmount = xpFraction;
+            if (delayTimer > 3)
+            {
+                lerpTimer += Time.deltaTime;
+                float percentComplete = lerpTimer / 4;
+                frontXPBar.fillAmount = Mathf.Lerp(FXP, backXPBar.fillAmount, percentComplete);
+            }
+        }
+    }
+    public void GainEXP(float xpGained)
+    {
+        currentXP += xpGained;
+        lerpTimer = 0f;
+        delayTimer = 0f;
+    }
+    public void LevelUp()
+    {
+        level++;
+        frontXPBar.fillAmount = 0;
+        backXPBar.fillAmount = 0;
+        currentXP = Mathf.RoundToInt(currentXP - requiredXP);
+        //Increase stats
+
+        requiredXP = CalculateRequiredXP();
+    }
+
+    private int CalculateRequiredXP()
+    {
+        int solveForRequiredXP = 0;
+        for (int levelCycle = 1; levelCycle <= level; levelCycle++)
+        {
+            solveForRequiredXP += (int)Mathf.Floor(levelCycle + additionMultiplier * Mathf.Pow(powerMultiplier,levelCycle / divisionMultiplier));
+        }
+        return solveForRequiredXP / 4;
+    }
+}
