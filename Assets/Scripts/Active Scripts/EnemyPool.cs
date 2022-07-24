ec966446f9f2943b9d6bc571dc8398c939430f66
srsbsns5 <elyzaescano@gmail.com>
2022-07-24 15:56:54 +0800

    Update EnemyPool.cs

--- Assets/Scripts/Active Scripts/EnemyPool.cs
+++ Assets/Scripts/Active Scripts/EnemyPool.cs
@@ -5,37 +5,32 @@ using UnityEngine.Pool;
 
 public class EnemyPool : MonoBehaviour
 {
-    [SerializeField] private PoolObjectEnemy goblinPrefab;
-    [SerializeField] private int spawnAmount; 
-    [SerializeField] private bool usePool; 
-    private ObjectPool<PoolObjectEnemy> goblinPool;
-
-    private void Start() 
+    public enum PoolType
     {
-        goblinPool = new ObjectPool<PoolObjectEnemy>(() => 
-        {   return Instantiate(goblinPrefab);   }, 
-            goblin => {goblin.gameObject.SetActive(true);},
-            goblin => {goblin.gameObject.SetActive(false);},
-            goblin => {Destroy(goblin.gameObject);},
-            true,
-            3,
-            100);
-            InvokeRepeating(nameof(Spawn), 0.2f, 0.2f);
-    }
+       Goblin,
+       Orc
+    } 
 
-    private void Spawn()
+    public PoolType poolType;
+    public bool collectionChecks = true;
+    public int maxPoolSize = 50;
+
+    public IObjectPool<GameObject> pool;
+
+    public IObjectPool<GameObject> enemyPool
     {
-        for(var i = 0; i < spawnAmount; i++)
+        get
         {
-            var goblin = usePool ? goblinPool.Get() : Instantiate(goblinPrefab);
-            goblin.transform.position = transform.position + Random.insideUnitSphere * 10;
-
+            if (enemyPool == null)
+            {
+                if (poolType == PoolType.Goblin)
+                {
+                    enemyPool = new ObjectPool<GameObject>
+                    (
+                        Instantiate()
+                    )
+                }
+            }
         }
-    }
-
-    private void KillGoblin(PoolObjectEnemy goblin)
-    {
-        if(usePool) goblinPool.Release(goblin);
-        else Destroy(goblin.gameObject);
     }
 }
