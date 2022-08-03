9c184ef2d05b9d65076c5fd8e5f8f6198191e402
Stacia <104016782+starchi3@users.noreply.github.com>
2022-08-03 14:04:39 +0800

    MainMenu
    it looks boring for now

---
+++ Assets/Scripts/Active Scripts/LoadScene.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("TestScene");
+
+    }
+
+    public void Instructions()
+    {
+        SceneManager.LoadScene("Instructions");
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+     public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit Game");
+    }
+
+}
