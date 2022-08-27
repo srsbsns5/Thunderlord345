e30aa1d081f469ed14887b95c26735dac5db6bb9
srsbsns5 <elyzaescano@gmail.com>
2022-08-27 23:00:29 +0800

    beginning of enemy attack

---
+++ Assets/Scripts/Active Scripts/EnemyAttack.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    public Enemy enemy;
+    private float rateofAttack;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
